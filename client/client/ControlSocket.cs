60fcae38e25fab5502263ccd3adfdfdc7ebb12e4
rpSebastian <cxxuhang@126.com>
2019-06-15 17:57:56 +0800

    init

---
+++ client/client/ControlSocket.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace client
+{
+    class ControlSocket
+    {
+        private static Byte[] buffer = new Byte[1024 * 1024 * 2];
+        private int bufferLength;
+        private Socket socket;
+        private String receiveMessage()
+        {
+            bufferLength = socket.Receive(buffer);
+            String message = Encoding.UTF8.GetString(buffer, 0, bufferLength);
+            Console.WriteLine(message);
+            return message;
+        }
+        public ControlSocket(IPAddress serverIp, int serverPort)
+        {
+            IPEndPoint ipe = new IPEndPoint(serverIp, 12345);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipe);
+            receiveMessage();
+        }
+        public void USER(String user)
+        {
+            socket.Send(Encoding.UTF8.GetBytes($"USER {user}\r\n"));
+            receiveMessage();
+        }
+        public void PASS(String pass)
+        {
+            socket.Send(Encoding.UTF8.GetBytes($"PASS {pass}\r\n"));
+            receiveMessage();
+        }
+        public int SIZE(String fname)
+        {
+            socket.Send(Encoding.UTF8.GetBytes($"SIZE {fname}\r\n"));
+            String message = receiveMessage();
+            int st = message.IndexOf(' ') + 1;
+            int ed = message.Length - 1;
+            return int.Parse(message.Substring(st, ed - st));
+        }
+        public void PWD()
+        {
+            socket.Send(Encoding.UTF8.GetBytes("PWD\r\n"));
+            receiveMessage();
+        }
+        public int PASV()
+        {
+            socket.Send(Encoding.UTF8.GetBytes("PASV\r\n"));
+            String message = receiveMessage();
+            int st = message.IndexOf('(') + 1;
+            int ed = message.IndexOf(')');
+            String[] number = message.Substring(st, ed - st).Split(new char[] { ',' });
+            return int.Parse(number[4]) * 256 + int.Parse(number[5]);
+        }
+        public void RETR(string fname)
+        {
+            socket.Send(Encoding.UTF8.GetBytes($"RETR {fname}\r\n"));
+            receiveMessage();
+        }
+        public void RETR_END()
+        {
+            receiveMessage();
+        }
+    }
+}
